edc3f68ea850f55c47ad444cdbdc6d5531f83cb9
hsinjungLee <144487630+hsinjungLee@users.noreply.github.com>
2023-09-26 15:23:31 -0400

    not finish

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,19 +2,26 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Step 1: Reference RecyclerView object
+        recyclerView = findViewById(R.id.recyclerView)
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //Step 2: Provide a LayoutManager
+        recyclerView.layoutManager = GridLayoutManager(this,2)
 
 
         //Step 4: Provide a RecyclerView.Adapter
+        recyclerView.adapter = NumberDisplayAdapter()
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,39 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
-
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
-
+class NumberDisplayAdapter (private val number : Array<Int>): RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>()/* Step 3a: Provide Adapter Parent */ {
     //Step 3b: Complete function definitions for adapter
+
+    class NumberViewHolder (val _textView: TextView) : RecyclerView.ViewHolder (_textView) {
+        val imageView = _textView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(
+           TextView(
+               parent.context
+           ).apply {
+               layoutParams =ViewGroup.LayoutParams(300,300)
+           }
+
+           )
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        return
+
+    }
+
+
 
 }
